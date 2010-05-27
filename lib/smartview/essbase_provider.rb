5ecb901877d3697bc13fb933b463862949309a49
Adam Gardiner <adam.b.gardiner@gmail.com>
2010-05-27 16:34:53 +0100

    Add support for alternate HFM filter syntax on Essbase provider
    The Essbase and HFM providers use a different syntax for filters.
    For the three filters that are common (Children, Descendants, and
    Base), we support the alternate HFM syntax on the Essbase provider.

--- lib/smartview/essbase_provider.rb
+++ lib/smartview/essbase_provider.rb
@@ -10,18 +10,33 @@ class SmartView
 
 
         # Parses a filter spec and returns a filter name and array of filter
-        # arguments.
+        # arguments. Essbase supports a different set of filters that also use
+        # a different syntax than the HFM filters. The lack of commonality with
+        # HFM for common operations is a pain.  Therefore, we also support
+        # the common filter operations via the alternate HFM syntax, e.g.
+        #   * {Q1.[Descendants]}
+        #   * {IS.[Children]}
+        #   * {Year.[Base]}
         def process_filter(dimension, filter_spec = nil)
             # Default filter is entire dimension
             result = ['Hierarchy', [dimension]]
             if filter_spec
-                # Retrieve list of filters for dimension, and search for one
-                # that matches the filter spec
-                filters = get_filters(dimension)
-                filters.each do |filter|
-                    if md = (filter.decompose && filter.decompose.match(filter_spec))
-                        result = [filter.name, md.captures]
-                        break
+                if filter_spec =~ /^\{(?:([\w\s]+)\.)?\[([\w\s]+)\]\}$/
+                    # Support a common filter syntax across providers - {(Member.)?[Filter]}
+                    mbr = $1 || dimension
+                    result = [$2, [mbr]]
+                    if $2 =~ /^Base$/i
+                        result = ['Level', [mbr, '0']]
+                    end
+                else
+                    # Retrieve list of filters for dimension, and search for one
+                    # that matches the filter spec
+                    filters = get_filters(dimension)
+                    filters.each do |filter|
+                        if md = (filter.decompose && filter.decompose.match(filter_spec))
+                            result = [filter.name, md.captures]
+                            break
+                        end
                     end
                 end
             end
